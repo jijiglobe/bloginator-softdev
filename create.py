e5371fbd3d6c516e807f4ff19a262c6544d6e8db
kahsoonyap <kahsoonyap1998@hotmail.com>
2015-10-12 13:41:20 -0400

    Worked with Ho Yin. Created the database

---
+++ create.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+conn = sqlite3.connect("blog.db")
+
+c = conn.cursor()
+
+q = "CREATE TABLE  user(uid integer, name text)"
+c.execute(q)
+
+q = "CREATE TABLE post(pid integer, uid integer, title text, content text)"
+c.execute(q)
+
+q ="CREATE TABLE comment(cid integer, pid integer, uid integer, content text)"
+c.execute(q)
+
+conn.commit()
