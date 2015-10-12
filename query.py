c1ecca4e18a11e1017d978cfd073bf5384fc62a7
kahsoonyap <kahsoonyap1998@hotmail.com>
2015-10-12 14:37:42 -0400

    Worked with Ho Yin. Created some query methods. Added some data to database.

--- query.py
+++ query.py
@@ -1,1 +1,30 @@
 import sqlite3
+
+conn = sqlite3.connect("blog.db")
+c = conn.cursor()
+
+
+def aPost():
+    allPosts="""
+    SELECT post.title, user.name, post.content
+    FROM post,user
+    WHERE post.uid=user.uid
+    """
+    result = c.execute(allPosts);
+    for r in result:
+        print (r)
+
+def uPost(uid):
+    userPosts="""
+    SELECT user.name, post.title, post.content
+    FROM post,user
+    WHERE user.uid=
+    """+str(uid)
+    result = c.execute(userPosts);
+    for r in result:
+        print (r)
+
+
+aPost()
+print ("\n\nUserposts")
+uPost(2)
