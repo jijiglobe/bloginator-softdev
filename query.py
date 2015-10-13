73e0032973c6c56b2fbbd4cffc4513760bce186a
elc1798 <subliminalmau5@gmail.com>
2015-10-13 11:45:52 -0400

    Fix stub

--- query.py
+++ query.py
@@ -1,22 +1,33 @@
 import sqlite3
 
-conn = sqlit3.connect("blog.db")
-c = conn.cursor()
+DB_NAME = "blog.db"
 
 # Returns: Nothing
 def register_user(username, password):
-    pass
+    conn = sqlite3.connect(DB_NAME)
+    c = conn.cursor()
+    # FILL ME IN
+    conn.commit()
+    conn.close()
 
 # Returns: a dictionary with the keys:
 #   "title"     A string with the title of the post
 #   "contents"  A string with the contents of the post
 def get_post(pid):
-    pass
+    conn = sqlite3.connect(DB_NAME)
+    c = conn.cursor()
+    # FILL ME IN
+    conn.commit()
+    conn.close()
 
 # Returns: a list of post ids made by the user specified by uid
 #   Each element of the list should be an integer containing the pid of the post
 def get_posts_by_user(uid):
-    pass
+    conn = sqlite3.connect(DB_NAME)
+    c = conn.cursor()
+    # FILL ME IN
+    conn.commit()
+    conn.close()
 
 # Returns: a list of dictionaries of all the posts
 #   Each element of the list should be a dictionary containing the keys:
@@ -24,14 +35,25 @@ def get_posts_by_user(uid):
 #       "comment_id"
 #       "commenter_id"
 def get_comments_for_post(pid):
-    pass
+    conn = sqlite3.connect(DB_NAME)
+    c = conn.cursor()
+    # FILL ME IN
+    conn.commit()
+    conn.close()
 
 # Returns: a list of comment ids made by the user specified by uid
 #   Each element of the list should be an integer that is the comment id
 def get_comments_for_user(uid):
-    pass
+    conn = sqlite3.connect(DB_NAME)
+    c = conn.cursor()
+    # FILL ME IN
+    conn.commit()
+    conn.close()
 
 # Returns a string containing the content for the comment specified by cid
 def get_comment_contents(cid):
-    pass
-
+    conn = sqlite3.connect(DB_NAME)
+    c = conn.cursor()
+    # FILL ME IN
+    conn.commit()
+    conn.close()
