a148830fec3f5ae54f8a68e85e1911068cb40896
sarahjoseph723 <sjoseph@stuy.edu>
2015-10-13 11:41:59 -0400

    added passes

--- app.py
+++ app.py
@@ -3,21 +3,21 @@ from flask import Flask, render_template, session
 @app.route("")
 @app.route("/home")
 def home():
-
+    pass
 
 @app.route("/login")
 def login():
-
+    pass
 
 @app.route("/profile") #list of all user posts + most recent comments
 def profile():
-
+    pass
 
 @app.route("/posts") #shows all posts, title + most recent comment
 def posts():
-
+    pass
 
 @app.route("/posts/<pid>") #for individual posts
 def onepost():
     #comments
-
+    pass
