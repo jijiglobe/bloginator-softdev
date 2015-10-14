bb4657d8fd60ad0d78862e1078d182944c3fe22d
hoyinho <hoyinho999@gmail.com>
2015-10-13 20:33:46 -0400

    added multiple query methods

--- query.py
+++ query.py
@@ -2,11 +2,51 @@ import sqlite3
 
 DB_NAME = "blog.db"
 
+#Returns next available pid
+def get_next_uid():
+    conn = sqlite3.connect(DB_NAME)
+    c = conn.cursor()
+    q = "SELECT uid FROM user"
+    allUid = c.execute(q)
+    conn.commit()
+    conn.close()
+    i = 0
+    while i == int(uid):
+        i+=1
+    return i
+
+#Returns next available cid
+def get_next_cid():
+    conn = sqlite3.connect(DB_NAME)
+    c = conn.cursor()
+    q = "SELECT cid FROM comment"
+    allCid = c.execute(q)
+    conn.commit()
+    conn.close()
+    i = 0
+    while i == int(uid):
+        i+=1
+    return i
+
+#Returns next available pid
+def get_next_pid():
+    conn = sqlite3.connect(DB_NAME)
+    c = conn.cursor()
+    q = "SELECT pid FROM post"
+    allPid = c.execute(q)
+    conn.commit()
+    conn.close()
+    i = 0
+    while i == int(uid):
+        i+=1
+    return i
+
 # Returns: Nothing
 def register_user(username, password):
     conn = sqlite3.connect(DB_NAME)
     c = conn.cursor()
-    # FILL ME IN
+    q = "INSERT INTO user values(" + str(get_next_uid())+ "," + username + "," + password + ")";
+    c.execute(q);
     conn.commit()
     conn.close()
 
@@ -16,18 +56,24 @@ def register_user(username, password):
 def get_post(pid):
     conn = sqlite3.connect(DB_NAME)
     c = conn.cursor()
-    # FILL ME IN
+    q = "SELECT title, content FROM post where pid=" + str(pid)
+    result = c.execute(q);
+    print(result)
+    post = {'title': result[0] , 'content': result[1]}
     conn.commit()
     conn.close()
+    return post
 
 # Returns: a list of post ids made by the user specified by uid
 #   Each element of the list should be an integer containing the pid of the post
 def get_posts_by_user(uid):
     conn = sqlite3.connect(DB_NAME)
     c = conn.cursor()
-    # FILL ME IN
+    q = "SELECT uid FROM post,user where post.uid=" + str(uid)
+    userPosts = c.execute(q)
     conn.commit()
     conn.close()
+    return userPosts
 
 # Returns: a list of dictionaries of all the posts
 #   Each element of the list should be a dictionary containing the keys:
@@ -37,9 +83,11 @@ def get_posts_by_user(uid):
 def get_comments_for_post(pid):
     conn = sqlite3.connect(DB_NAME)
     c = conn.cursor()
-    # FILL ME IN
+    q = "SELECT username, cid, comment.uid FROM post,user,comment WHERE comment.pid = post.pid and comment.pid =" + str(pid)
+    postComments = c.execute(q)
     conn.commit()
     conn.close()
+    return postComments
 
 # Returns: a list of comment ids made by the user specified by uid
 #   Each element of the list should be an integer that is the comment id
@@ -57,3 +105,7 @@ def get_comment_contents(cid):
     # FILL ME IN
     conn.commit()
     conn.close()
+
+"""print(get_post(1))
+print(get_posts_by_uid(1))"""
+print(get_next_uid)
