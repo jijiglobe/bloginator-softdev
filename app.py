82f0f9ceef7f3f346e786e7bc1cd30958de24cf3
sarahjoseph723 <sjoseph@stuy.edu>
2015-10-16 11:47:20 -0400

    pid stuff on posts

--- app.py
+++ app.py
@@ -41,56 +41,72 @@ def register():
 
 @app.route("/profile/<uid>", methods=["GET","POST"]) #list of all user posts + most recent comments
 def profile():
-    
-    #how do you validate this? what if it is an invlaid number or something
     postList = query.get_posts_by_user(session['UID']) #post ids
-    retList = []
+    postDict = {}
     ctr = 0
     while ctr < len(postList):
         d = query.get_post(postList[ctr]) # {"title":-, "contents":-, "username":-,}
+        title = d['title']
         listOfComments = query.get_comments_for_post(postList[ctr])
         lastInd = len(listOfComments) -1
         topCommentID = listOfComments[lastInd]["comment_id"]
         topCommentString = query.get_comment_contents(topCommentID)
-        d['last_comment'] = topCommentString
-        retList.append(d)
+        postDict[postLIst[ctr]] = {'title':title, 'topComment':topCommentString}
         ctr += 1
     #retList in the format [ {"title":,"contents":,"username":,"last_comment":}, {...}]
-    return render_template("userposts.html", POST_LIST = retlist)
+    return render_template("userposts.html", POST_LIST = postDict)
     
 #add new posts here
 @app.route("/posts") #shows all posts, title + most recent comment. 
 def posts():
     postList = query.get_post_list(); #list of all pid's
-    listOfDicts = []
+    postDict = {}
     ctr = 0;
     while ctr < len(postList):
         title = query.get_post(postList[ctr])["title"] 
         #content = query.get_post(postList[ctr])["contents"]
         topCommentID = query.get_comments_for_post(postList[ctr])[0]["comment_id"]
         topComment = query.get_comment_contents(topCommentID)
-        postDict[title]=topComment
+        postDict[postList[ctr]]={'title':title,'topComment':topComment}
         ctr += 1
-    return render_template("allposts.html", POST_DICT = postDict)#, PIDS_LIST = postList) 
-
-@app.route("/posts/<pid>", methods = ["GET","POST"]) #for individual posts
-def onepost(pid=0):
     if request.method == "GET":
-        commentList = query.get_comments_for_post(pid) #actually a dicitonary
-        ctr = 0;
-        while ctr < len(commentList):
-            x = query.get_comment_contents(commentList[ctr]["comment_id"])
-            commentList[ctr] = x
-            ctr += 1
-        #commentList is now a list of strings of the contents
-        pid = int(pid)
-        #check for valid pid somehow?
-        return render_template("post.html", POST_CONTENT = query.get_post(pid), COMMENT_LIST = commentList)
+        #postDict format: {pid: {'title':, 'topComment':}, pid:{...}, ...}
+        return render_template("allposts.html", POST_DICT = postDict)
     else:
         assert(request.method == "POST")
-        query.add_comment(request.form['comment_content'])
-        return redirect(url_for("posts/<pid>"))
+        if not session['logged_in']:
+            return render_template("allposts.html", POST_DICT = postDict, error = "Please log in before you post!")
+        else:
+            if (request.POST.get('newpost')):#submit button for new post
+                addPost(session["UID"],request.form['title'], request.form['content'])
+                return redirect(url_for("posts"))
 
+        
+@app.route("/posts/<pid>", methods = ["GET","POST"]) #for individual posts
+def onepost(pid=-1):
+    pid = int(pid)
+    if len(get_post(pid)) == 0:
+            return redirect(url_for("posts"))
+    else:
+        if request.method == "GET":
+            commentList = query.get_comments_for_post(pid) #actually a dicitonary
+            ctr = 0;
+            while ctr < len(commentList):
+                x = query.get_comment_contents(commentList[ctr]["comment_id"])
+                commentList[ctr] = x
+                ctr += 1
+        #commentList is now a list of strings of the contents
+            return render_template("post.html", POST_CONTENT = query.get_post(pid), COMMENT_LIST = commentList)
+        else:
+            assert(request.method == "POST")
+            if (request.POST.get('add_comment')): #submit button for comment
+                query.addComment(session['UID'],pid,request.form['comment_content'])
+                return redirect(url_for("posts/<pid>"))
+            if (request.POST.get('del_post')): #submit button for delete
+                if session['UID'] == query.get_uid_from_post(pid):
+                    query.delPost(pid)
+                return redirect(url_for("posts"))
+                
 
 if __name__ == "__main__":
     app.debug = True
